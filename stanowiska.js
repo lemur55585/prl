c9f5a0c402aa0d329fb74ae36e6955fb244c5ec7
lemur55585 <jancch2013@gmail.com>
2026-09-25 19:31:15 +0200

    Add files via upload

---
+++ stanowiska.js
@@ -0,0 +1,33 @@
+// --- STANOWISKA I KADENCJE ---
+const nazwaSekretarza1     = "Pax";[cite: 12]
+const dataStartuKadencji1  = "2026-09-05T00:00:00";[cite: 12]
+const dataKoncaKadencji1   = "2026-10-31T23:59:59";[cite: 12]
+
+const nazwaSekretarza2     = "Devon";[cite: 12]
+const dataStartuKadencji2  = "2026-09-05T00:00:00";[cite: 12]
+const dataKoncaKadencji2   = "2026-10-31T23:59:59";[cite: 12]
+
+// --- BAZA PRAWNA ---
+const nazwaKonstytucjiTekst   = "Konstytucja PRL z 2026";[cite: 12]
+const dataNowelizacjiTekst    = "01.08.2026";[cite: 12]
+const statusPrawnyAktywny     = true;[cite: 12]
+const czyTrwaGlosowanie       = false;[cite: 12]
+
+// --- REJESTR ODZNACZEŃ (ORDER ORŁA CZERWONEGO) ---
+const listaOdznaczonychObywateli = [
+  { imie: "Maloy", powod: "Za utworzenie serwera i ogromną aktywność", data: "" },[cite: 12]
+  { imie: "Pas",   powod: "Za bycie z nami od samych początków i dalszego uczestnictwa w życiu serwera", data: "" },[cite: 12]
+  { imie: "Devon", powod: "Za rozbudowę serwera i zostanie pierwszą osobą na stanowisku sekretarza po Maloyu", data: "" },[cite: 12]
+  { imie: "Pax",   powod: "Za współzałożenie pierwszego serwera i pomaganie w następnych", data: "" },[cite: 12]
+  { imie: "Eko",   powod: "Za znaczący udział przy tworzeniu PRL3, dodanie na serwer wielu nowych osób i organizację eventu z pisania konstytucji", data: "" }[cite: 12]
+];
+
+// --- ZARZĄDCY UCZELNI ---
+const listaZarzadcowUczelni = [
+  { imie: "Brak zarządców", powod: "Wydział nieobsadzony.", data: "" }[cite: 14]
+];
+
+// --- WYKŁADOWCY ---
+const listaWykladowcow = [
+  { imie: "Brak wykładowców", powod: "Brak przypisanych profili.", data: "" }[cite: 14]
+];
